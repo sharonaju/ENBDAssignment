f925d332d4b71d17baf0fe00956b6698f252cc51
Sharon <sharonbovas@gmail.com>
2020-03-22 08:22:40 +0400

    Integrated UI for list view

---
+++ ENBDAssignment/ENBDAssignment/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  ENBDAssignment
+//
+//  Created by Sharon on 22/03/2020.
+//  Copyright © 2020 Sharon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- ENBDAssignment/ENBDAssignment/View/PhotoListViewController.swift
+++ ENBDAssignment/ENBDAssignment/View/PhotoListViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class PhotoListViewController: UIViewController {
-
+class PhotoListViewController: UIViewController, UISearchBarDelegate {
+    @IBOutlet weak var searchTextField: UISearchBar!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.register(UINib(nibName: "PhotoCell", bundle: .main), forCellWithReuseIdentifier: "PhotoCell")
+        searchTextField.text = "Apple"
     }
 
 
+}
+extension PhotoListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let cell = collectionView
+           .dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+         cell.backgroundColor = .black
+
+                return cell
+    }
+    
+    
+    
 }
 
